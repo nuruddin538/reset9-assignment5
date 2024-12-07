fe424ad69fe19a0314284e9b5c99386c9f8bc118
Nur Uddin <nuruddinnur538@gmail.com>
2024-12-07 13:18:54 +0600

    donation and history section added successfully

--- js/index.js
+++ js/index.js
@@ -1,3 +1,29 @@
 function navigateToBlog() {
     window.location.href = 'blog.html';
 }
+
+// utilitis function
+function getInputValueById(id) {
+    return parseFloat(document.getElementById(id).value);
+}
+
+
+
+const donationTab = document.getElementById("donation-tab");
+const historyTab = document.getElementById("history-tab")
+
+historyTab.addEventListener("click", function(){
+    historyTab.classList.add(
+        "bg-[#B4F461]",
+        "text-gray-700"
+    )
+    donationTab.classList.remove("bg-[#B4F461]")
+    document.getElementById("donate-card").classList.add("hidden");
+    document.getElementById("history-section").classList.remove("hidden");
+})
+donationTab.addEventListener("click", function(){
+    donationTab.classList.add("bg-[#B4F461]", "text-gray-700");
+    historyTab.classList.remove("bg-[#B4F461]");
+    document.getElementById("donate-card").classList.remove("hidden");
+    document.getElementById("history-section").classList.add("hidden");
+})
